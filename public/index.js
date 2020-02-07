6a81b035b668ffb3222bf851f9ac7d6d7b8be377
kj <kristoferjoseph@gmail.com>
2020-02-07 10:51:49 -0800

    Adds auth routes.

--- public/index.js
+++ public/index.js
@@ -1,1 +1,139 @@
-console.log('Hello world from client-side JS!')
+(function init() {
+  const authEl = document.getElementById('auth')
+  const accountEl = document.getElementById('account')
+  const avatarEl = document.getElementById('avatar')
+  fetch('/auth')
+    .then(response => response.json())
+    .then(result => {
+      let accountData = result.account
+      let href = result.href
+      authEl.innerHTML = accountData
+        ? logout()
+        : login(href)
+      accountEl.innerHTML = accountData
+        ? account(accountData)
+        : accountEl.innerHTML
+      avatarEl.innerHTML = avatar(accountData.avatar)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+
+  function account(data) {
+    data = data || {}
+    let name = data.name
+    let username = data.login
+    let github = data.url
+    return `
+  <div
+    class="
+      display-flex
+    "
+  >
+    <span
+      style="
+        min-width: 5rem;
+      "
+    >
+      Name
+    </span>  <span
+      style="font-weight: 100"
+    >
+      ${name}
+    </span>
+  </div>
+  <div
+    class="display-flex"
+    style="
+      margin-bottom: 1rem;
+    "
+  >
+    <span
+      style="
+        min-width: 5rem;
+      "
+    >
+      User
+    </span>  <span
+      style="font-weight: 100"
+    >
+      ${username}
+    </span>
+  </div>
+  <!--
+  <a
+    href="${github}"
+    style="
+      color: var(--blue);
+      text-decoration: none;
+      text-align: center;
+    "
+  >
+    GitHub Profile →
+  </a>
+  -->
+    `
+  }
+
+  function avatar(src) {
+    return `
+   <img src="${src}" alt="User's avatar">
+    `
+  }
+
+  function login(href) {
+    return `
+  <a
+    href=${href}
+    class="
+      linear-gradient2
+      font-size1
+      color-light
+    "
+    style="
+      padding-top: 1rem;
+      padding-right: 3rem;
+      padding-bottom: 1rem;
+      padding-left: 3rem;
+      font-size: 1.75rem;
+      text-decoration: none;
+      border-radius: 0.25rem;
+      border: none;
+      cursor: pointer;
+    "
+  >Login with GitHub</a>
+    `
+  }
+
+  function logout() {
+    return `
+  <form
+    method=POST
+    action=/logout
+    style="
+      margin-top: 0.5rem;
+    "
+  >
+    <button
+      class="
+        linear-gradient2
+        color-light
+      "
+      style="
+        padding-top: 1rem;
+        padding-right: 3rem;
+        padding-bottom: 1rem;
+        padding-left: 3rem;
+        font-size: 1.75rem;
+        text-decoration: none;
+        border-radius: 0.25rem;
+        border: none;
+        cursor: pointer;
+      "
+    >
+      Logout
+    </button>
+  </form>
+    `
+  }
+}())
--- src/http/get-auth/index.js
+++ src/http/get-auth/index.js
@@ -1,39 +1,20 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
+const arc = require('@architect/functions')
 
-let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
-  </head>
-  <body>
-    <h1 class="center-text">
-      Hello world!
-    </h1>
-    <p class="center-text">
-      Your new route is ready to go!
-    </p>
-    <p class="center-text">
-      Learn more about building <a href="https://docs.begin.com/en/functions/http/" class="link" target="_blank">Begin HTTP functions here</a>.
-    </p>
-  </body>
-</html>
-`
+async function auth(req) {
+  let account = req.session &&
+    req.session.account
+  console.log('SESSION: ', req.session)
+  let clientID = process.env.GITHUB_CLIENT_ID
+  let redirectURL = process.env.GITHUB_REDIRECT
+  let href = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_url=${redirectURL}`
+  let payload = {
+    account,
+    href
+  }
 
-// HTTP function
-exports.handler = async function http(req) {
-  console.log(req)
   return {
-    headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body: html
+    body: JSON.stringify(payload)
   }
 }
+
+exports.handler = arc.http.async(auth)
